a1a1bef6033d5c34e7d863705f3094d62e6df402
eugenefoxx <eugenefrompechora@gmail.com>
2021-03-03 14:58:20 +0300

    v1.1.968 edit style

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -21,6 +21,29 @@ function myFunction() {
     }
 }
 
+function myFunctionT() {
+    var input, filter, table, tr, td, cell, i, j;
+    input = document.getElementById("myInput2");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable2");
+    tr = table.getElementsByTagName("tr");
+    for (i = 1; i < tr.length; i++) {
+        // Hide the row initially.
+        tr[i].style.display = "none";
+
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+            cell = tr[i].getElementsByTagName("td")[j];
+            if (cell) {
+                if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                }
+            }
+        }
+    }
+}
+
 function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("myTable");
